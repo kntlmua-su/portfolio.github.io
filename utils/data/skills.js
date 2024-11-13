43b4a0b55823e5cf2090f29203139086e7959c11
kntlmua-su <133786668+kntlmua-su@users.noreply.github.com>
2024-11-13 13:12:58 +0700

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -6,19 +6,19 @@ export const skillsData = [
   'React',
   'Next JS',
   'Tailwind',
-  'MongoDB',
-  'MySQL',
-  'PostgreSQL',
+  'C++',
+  'Canva',
+  'Blender',
   'Git',
   'AWS',
-  'Bootstrap',
+  'Picsart',
   'Docker',
   'Go',
   'Figma',
-  'Firebase',
+  'Premiere Pro',
   'MaterialUI',
-  'Nginx',
-  'Strapi'
+  'Lightroom',
+  'Wordpress'
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
