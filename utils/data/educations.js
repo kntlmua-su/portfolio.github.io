e08b1e8dbcc59e212063828898c3ce28ab2b96c6
kntlmua-su <133786668+kntlmua-su@users.noreply.github.com>
2024-11-13 14:11:24 +0700

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,18 +3,18 @@ export const educations = [
     id: 1,
     title: "SMKN 1 Sooko",
     duration: "2021 - 2025",
-    institution: "National University of Bangladesh",
+    institution: "Vocational secondary education",
   },
   {
     id: 2,
     title: "SMPN 2 Sooko",
     duration: "2018 - 2021",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    institution: "Advanced basic education unit that provides general education",
   },
   {
     id: 3,
     title: "SDN 1 Wringinrejo",
     duration: "2012 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    institution: "elementary school",
   }
 ]
