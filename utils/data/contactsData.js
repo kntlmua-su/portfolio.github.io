f3180159084df890b7e7c1314f561210009f3fab
kntlmua-su <133786668+kntlmua-su@users.noreply.github.com>
2024-11-13 13:08:18 +0700

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,10 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    email: 'fahmiadit33@gmail.com',
+    phone: '(+62)81333619444',
+    address: 'Earth ',
+    github: 'https://github.com/kntlmua-su',
+    facebook: 'https://www.facebook.com/Fahmiaditya99/',
+    linkedIn: 'https://id.linkedin.com/in/fahmi-aditya%E3%82%B7-076478338/',
+    twitter: 'https://x.com/Fahmiaditya99?t=3f8IvDPyIdDtQzVVuNhnnw&s=09',
     devUsername: "said7388"
 }
