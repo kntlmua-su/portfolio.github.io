ba9234ee676190aaa6432271c57376c2531c30f6
kntlmua-su <133786668+kntlmua-su@users.noreply.github.com>
2024-11-13 13:20:32 +0700

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
+    title: "SMKN 1 Sooko",
+    duration: "2021 - 2025",
     institution: "National University of Bangladesh",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
+    title: "SMPN 2 Sooko",
+    duration: "2018 - 2021",
     institution: "Noakhali Islamia Kamil Madrasah",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
+    title: "SDN 1 Wringinrejo",
+    duration: "2012 - 2018",
     institution: "Baitus Saif Islamia Madrasah",
   }
 ]
