7c724b24f8cbc3cac1b8cca1ed7b100c0e01a4c2
kntlmua-su <133786668+kntlmua-su@users.noreply.github.com>
2024-11-13 13:26:10 +0700

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -6,5 +6,5 @@ export const contactsData = {
     facebook: 'https://www.facebook.com/Fahmiaditya99/',
     linkedIn: 'https://id.linkedin.com/in/fahmi-aditya%E3%82%B7-076478338/',
     twitter: 'https://x.com/Fahmiaditya99?t=3f8IvDPyIdDtQzVVuNhnnw&s=09',
-    devUsername: "said7388"
+    devUsername: "fahmi aditya"
 }
