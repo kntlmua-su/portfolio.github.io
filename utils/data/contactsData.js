87d417478f347e80a68e50ade751f3bd6bdafd6a
kntlmua-su <133786668+kntlmua-su@users.noreply.github.com>
2025-06-03 10:48:50 +0700

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,10 +1,10 @@
 export const contactsData = {
-    email: 'fahmiadit33@gmail.com',
-    phone: '(+62)81333619444',
+    email: 'fahmiaditya68718@gmail.com',
+    phone: '(+62)881080800321',
     address: 'Earth ',
     github: 'https://github.com/kntlmua-su',
     facebook: 'https://www.facebook.com/Fahmiaditya99/',
     linkedIn: 'https://id.linkedin.com/in/fahmi-aditya%E3%82%B7-076478338/',
     twitter: 'https://x.com/Fahmiaditya99?t=3f8IvDPyIdDtQzVVuNhnnw&s=09',
-    devUsername: "fahmi aditya"
+    devUsername: "Fahmi aditya"
 }
